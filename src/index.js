4985fc18a1cf50f47fb6110097fa5cc37f588374
VladaRey <vladapodzirey@gmail.com>
2024-07-18 19:44:01 +0300

    Added the footer and some css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import './index.css';
+
 import Weather from "./Weather";
 
 const rootElement = document.getElementById("root");
@@ -10,5 +12,8 @@ root.render(
   <StrictMode>
     <h1>Weather App</h1>
     <Weather />
+    <div className="footer">
+      <a href="" target="blank_">Open source code,</a> by Vladyslava Podzirei
+    </div>
   </StrictMode>
 );
