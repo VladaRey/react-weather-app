da05aa876ea6695b81e91f5a46ca8e5a53ae9a1e
VladaRey <vladapodzirey@gmail.com>
2024-07-22 09:36:07 +0300

    Added basic stylling

--- src/Weather.js
+++ src/Weather.js
@@ -6,24 +6,43 @@ export default function Weather() {
     return (
       <div className="Weather">
         <form>
-          <input type="search" placeholder="Enter a city..." autofocus="" />
-          <input type="submit" value="Submit" className="btn btn-primary"/>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                autofocus="on"
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Submit"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
         </form>
         <h1>New York</h1>
         <ul>
           <li>Wednesday 09:00</li>
           <li>Mostly cloudy</li>
-          <div className="row">
+          <div className="row mt-3">
             <div className="col-6">
-              <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="icon"/>
-              24° C
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                alt="icon"
+              />
+              <span className="temperature">11</span>
+              <span className="unit">°C</span>
             </div>
             <div className="col-6">
-                <ul>
-                    <li>Precipitation: 15%</li>
-                    <li>Humidity: 72%</li>
-                    <li>Wind: 13 km/h</li>
-                </ul>
+              <ul>
+                <li>Precipitation: 15%</li>
+                <li>Humidity: 72%</li>
+                <li>Wind: 13 km/h</li>
+              </ul>
             </div>
           </div>
         </ul>
