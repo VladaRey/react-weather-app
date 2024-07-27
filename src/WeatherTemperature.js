ba6fd7d70fa29d6ad8cf9512bb563a5eb5d9b483
VladaRey <vladapodzirey@gmail.com>
2024-07-27 13:57:43 +0300

    use the correct formula

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -14,7 +14,7 @@ export default function WeatherTemperature(props) {
   }
 
   function fahrenheit() {
-    return (props.celsius * 9) + 32;
+    return (props.celsius * 9) /5 + 32;
   }
 
   if (unit === "celsius") {
