023a3b0070c395ea3002e945cf5d9a47f186b644
VladaRey <vladapodzirey@gmail.com>
2024-07-27 13:51:07 +0300

    made the unit coversion

---
+++ src/WeatherTemperature.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+  let [unit, setUnit] = useState("celsius");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return (props.celsius * 9) + 32;
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div>
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          °C |{" "}
+          <a href="/" rel="noreferrer" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <span className="temperature">{Math.round(fahrenheit())}</span>
+        <span className="unit">
+          <a href="/" rel="noreferrer" onClick={showCelsius}>
+            °C
+          </a>{" "}
+          | °F
+        </span>
+      </div>
+    );
+  }
+}
